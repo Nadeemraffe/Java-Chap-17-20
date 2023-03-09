5a1cfd3994c15c5f1f54764f7fe054741694a298
Nadeemraffe <123829445+Nadeemraffe@users.noreply.github.com>
2023-03-09 19:31:27 +0500

    Add files via upload

---
+++ Javascript17-20/app.js
@@ -0,0 +1,108 @@
+//Chapter 14-16
+
+//Question 1
+//var students = [];
+
+//Question 2
+//var students = new Array ();
+
+//Question 3
+//var name = ["Mudassir"];
+
+//Question 4
+//var num = [2];
+
+//Question 5
+//var boolean = [15>10];
+
+//Question 6
+//var mixed = ["Mudassir", 2, a>b];
+
+//Question 7
+//var qualifications = ["Qualifications:", "<ol>" + "<li>" + "SSC" + "</li>" , "<li>" + "HSC" + "</li>" , "<li>" + "BCS" + "</li>", "<li>" + "BS" + "</li>" , "<li>" + "BCOM" + "</li>" , "<li>" + "MS" + "</li>" , "<li>" + "M. Phil" + "</li>" , "<li>" + "PhD" + "</li>" + "</ol>"];
+//document.write(qualifications);
+
+//Question 8
+// var stdnm = ["Michael ", "John ", "Tony "];
+// var stdscr = [320 , 230 , 480 ];
+// var ttlmrks = 500;
+// var percentage1 = stdscr[0] / ttlmrks *100;
+// var percentage2 = stdscr[1] / ttlmrks *100;
+// var percentage3 = stdscr[2] / ttlmrks *100;
+// document.write(stdnm[0], stdscr[0] + " " + percentage1 + "% ");
+// document.write(stdnm[1], stdscr[1] + " " + percentage2 + "% ");
+// document.write(stdnm[2], stdscr[2] + " " + percentage3 + "% ");
+
+//Question 9
+// q.1.
+// var clrnm = [];
+// document.write(clrnm);
+// a.
+// clrnm = [prompt("What color you want to add to the beginning?")];
+// document.write(clrnm);
+// b.
+// clrnm = [prompt("What color you want to add to the end?")]
+// document.write(clrnm);
+// c.
+// clrnm.unshift("black", "green");
+// document.write(clrnm);
+// d.
+// clrnm.shift(0);
+// document.write(clrnm);
+// e.
+// clrnm.pop(0,1);
+// document.write(clrnm);
+// f.
+// clrnm = [prompt("Which index do you want add a color & color name?")];
+// clrnm.splice(clrnm);
+// document.write(clrnm);
+// g.
+// clrnm = [prompt("Which index do you want to delete color(s) & how many colors do you want to delete?")];
+// clrnm.splice(clrnm);
+// document.write(clrnm);
+
+//Question 10
+//var stdscr = [320, 230, 480, 120].sort();
+//document.write(stdscr);
+
+//Question 11
+//var cities = ["Karachi", "Lahore", "Islamabad", "Peshawar", "Quetta"];
+//var selectedCities = [cities[0] , cities[2], cities[4]];
+//document.write(selectedCities);
+
+//Question 12
+//var arr = ["This", "is", "my", "cat"];
+//var rmvcma = arr.join(' ');
+//document.write(rmvcma);
+
+//Question 13 ------
+// var arr = [];
+// arr.push('keyboard');
+// arr.push('mouse');
+// arr.push('printer');
+// arr.push('monitor');
+// document.write(arr.shift() + "<br />");
+
+// document.write(arr.shift() + "<br />");
+
+// document.write(arr.shift() + "<br />");
+
+// document.write(arr.shift() + "<br />"); 
+
+//Question 14 ------- 
+// var arr = [];
+// arr.push('keyboard');
+// arr.push('mouse');
+// arr.push('printer');
+// arr.push('monitor');
+// document.write(arr.pop() + "<br />");
+
+// document.write(arr.pop() + "<br />");
+
+// document.write(arr.pop() + "<br />");
+
+// document.write(arr.pop() + "<br />");
+
+//Question 15
+//var phnmnfctr = ["Apple" , "Samsung", "Motorolla", "Nokia", "Sony", "Haier"];
+//document.write("<select><option>"+phnmnfctr[0]+"</option><option>"+phnmnfctr[1]+"</option><option>"+phnmnfctr[2]+"</option><option>"+phnmnfctr[3]+"</option><option>"+phnmnfctr[4]+"</option><option>"+phnmnfctr[5]+"</option></select>");
